447f8a26d0984dcd1726508dfcb08d9cfb707b5b
Maximiliano Garnier Villarreal <maxgav13@gmail.com>
2018-05-10 16:38:43 -0600

    Added confidence interval for difference in means for known pop standard deviation and fixed confidence interval for difference in means for unknown pop standard deviation

--- R/ci_t2.R
+++ R/ci_t2.R
@@ -1,4 +1,4 @@
-#' @title Confidence interval for difference of the mean between 2 samples
+#' @title Confidence interval for difference of the mean between 2 samples (unknown sigmas)
 #' @description Calculates the confidence interval for the difference of the mean between 2 samples (t distribution).
 #' @param x1 Sample's 1 mean
 #' @param s1 Sample's 1 standard deviation
@@ -6,6 +6,7 @@
 #' @param x2 Sample's 2 mean
 #' @param s2 Sample's 2 standard deviation
 #' @param n2 Sample size of sample 2
+#' @param var.equal Logical indicating if the variances are equal or not
 #' @param conf.level Confidence level to use for the confidence interval (Default is 0.95)
 #' @export
 #' @return A data frame with the difference of means, degrees of freedom, and lower and upper ends of the confidence interval
@@ -19,12 +20,19 @@
 #' n2 <- 30
 #' ci_t2(x1, s1, n1, x2, s2, n2)
 #'
-ci_t2 <- function (x1, s1, n1, x2, s2, n2, conf.level = .95){
+ci_t2 <- function (x1, s1, n1, x2, s2, n2, var.equal = TRUE, conf.level = .95){
   alpha <- 1 - conf.level
-  stderr <- sqrt(s1^2/n1 + s2^2/n2)
-  upper <- (x1-x2) + qt(1-alpha/2, df = n1+n2-2) * stderr
-  lower <- (x1-x2) + qt(alpha/2, df = n1+n2-2) * stderr
-  DF <- data.frame(mean_diff = round(x1-x2, 2), df = n1+n2-2,
+  if (var.equal == T) {
+    stderr <- sqrt(((n1-1)*s1^2 + (n2-1)*s2^2)/(n1+n2-2)) * sqrt(1/n1 + 1/n2)
+    v <- n1+n2-2
+  } else {
+    stderr <- sqrt(s1^2/n1 + s2^2/n2)
+    v <- (s1^2/n1 + s2^2/n2)^2 / (((s1^2/n1)^2)/(n1-1) + ((s2^2/n2)^2)/(n2-1))
+  }
+
+  upper <- (x1-x2) + qt(1-alpha/2, df = v) * stderr
+  lower <- (x1-x2) + qt(alpha/2, df = v) * stderr
+  DF <- data.frame(mean_diff = round(x1-x2, 2), df = round(v, 2),
                    lower = round(lower, 2), upper = round(upper, 2))
   DF
 }
---
+++ R/ci_z2.R
@@ -0,0 +1,30 @@
+#' @title Confidence interval for difference of the mean between 2 samples (known sigmas)
+#' @description Calculates the confidence interval for the difference of the mean between 2 samples (Z distribution).
+#' @param x1 Sample's 1 mean
+#' @param sig1 Population's 1 standard deviation
+#' @param n1 Sample size of sample 1
+#' @param x2 Sample's 2 mean
+#' @param sig2 Population's 2 standard deviation
+#' @param n2 Sample size of sample 2
+#' @param conf.level Confidence level to use for the confidence interval (Default is 0.95)
+#' @export
+#' @return A data frame with the difference of means, and lower and upper ends of the confidence interval
+#' @import stats
+#' @examples
+#' x1 <- 42
+#' sig1 <- 8
+#' n1 <- 75
+#' x2 <- 36
+#' sig2 <- 6
+#' n2 <- 50
+#' ci_z2(x1, sig1, n1, x2, sig2, n2)
+#'
+ci_z2 <- function (x1, sig1, n1, x2, sig2, n2, conf.level = .95){
+  alpha <- 1 - conf.level
+  stderr <- sqrt(sig1^2/n1 + sig2^2/n2)
+  upper <- (x1-x2) + qnorm(1-alpha/2) * stderr
+  lower <- (x1-x2) + qnorm(alpha/2) * stderr
+  DF <- data.frame(mean_diff = round(x1-x2, 2),
+                   lower = round(lower, 2), upper = round(upper, 2))
+  DF
+}
