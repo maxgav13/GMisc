4dfb474b8224ec415bd0af0b1559d8536755ed1f
Maximiliano Garnier Villarreal <maxgav13@gmail.com>
2019-05-27 16:13:13 -0600

    Fixed typo in post_summary

--- R/post_summary.R
+++ R/post_summary.R
@@ -1,6 +1,6 @@
 #' @title Summary of vectors, matrices or data frames
-#' @description Summarises vectors, matrices or data frames by showing their mean, gometric mean, median, standard deviation, median absolute deviation,inter quartile range, and desired quantiles.
-#' @param x A vector, matrix or data frame
+#' @description Summarises vectors or data frames by showing their mean, gometric mean, median, standard deviation, median absolute deviation,inter quartile range, coefficient of variation, and desired quantiles.
+#' @param x A vector or data frame
 #' @param probs Desired quantiles to display
 #' @param digits Number of significant digits to display
 #' @export
@@ -10,14 +10,12 @@
 #' @import DescTools
 #' @examples
 #' x1 <- rnorm(100, 30, 6)
-#' x2 <- matrix(x1, nrow = 10)
-#' x3 <- data.frame(N = rnorm(100, 30, 6), T = rt(100, 10, 30))
+#' x2 <- data.frame(N = rnorm(100, 30, 6), T = rt(100, 10, 30))
 #' post_summary(x1)
 #' post_summary(x2)
-#' post_summary(x3)
 #'
 post_summary <- function(x, probs = seq(0,1,.25), digits = 3){
-  if (is.data.frame(x) == TRUE | is.matrix(x) == TRUE) {
+  if (is.data.frame(x) == TRUE) {
     x = select_if(x, is.numeric)
     res = rbind.data.frame(mean = apply(x, 2, mean, na.rm = T),
                            Gmean = apply(x, 2, Gmean, na.rm = T),
@@ -29,7 +27,7 @@ post_summary <- function(x, probs = seq(0,1,.25), digits = 3){
                            apply(x, 2, quantile, probs = probs, na.rm = T))
   } else if (is.vector(x) == TRUE) {
     res <- c(mean = mean(x, na.rm = T),
-             Gmean = apply(x, 2, Gmean, na.rm = T),
+             Gmean = Gmean(x, na.rm = T),
              median = median(x, na.rm = T),
              sd = sd(x, na.rm = T),
              mad = mad(x, na.rm = T),
