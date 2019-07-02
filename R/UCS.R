715d557a7a2f77df4a61fe4c1c1853354606f53c
Maximiliano Garnier Villarreal <maxgav13@gmail.com>
2019-07-02 16:39:34 -0600

    Fixed problem in pval_chisq, added more packages, and created new UCS (Unconfined Compressive Strength) function

---
+++ R/UCS.R
@@ -0,0 +1,73 @@
+#' @title Uniaxial Compressive Strength (UCS)
+#' @description Calculates different possible values of UCS based on point load test measurements. It performs several regressions (Gaussian and robust), and also calculates corrected value based on the point load index.
+#' @param W A vector of sample widths, in mm
+#' @param D A vector of sample diameters, in mm
+#' @param P A vector of loads at failure for each sample, in kN
+#' @param digits Number of significant digits to display (Default is 2)
+#' @export
+#' @return A comparison table and graph
+#' @references ASTM (1995). Standard Test Method for Determination of the Point Load Strength Index of Rock.
+#' @import stats
+#' @import dplyr
+#' @import MASS
+#' @import ggplot2
+#' @name UCS
+#' @examples
+#' W = c(70.0,95.0,80.0,90.0,98.0,110.0,100.0,100.0,110.0)
+#' D = c(40,55,60,50,25,60,40,70,50)
+#' P = c(4.3,3.4,4.5,3.1,5.0,6.4,2.6,11,4.1)
+#' UCS(W = W, D = D, P = P)
+#'
+UCS = function(W, D, P, digits = 2) {
+
+  A = W * D
+  De2 = 4 * A / pi
+  De = sqrt(De2)
+
+  dat.UCS = data.frame(P, De)
+  dat.50 = data.frame(De = 50)
+
+  I = P / De2 * 1000
+  f = (De / 50)^.45
+  I50 = I * f
+
+  # regresion t
+  # fit.t = heavyLm(P ~ De, data = dat.UCS, family = slash(df = nu))
+  # summary(fit.t)
+  # P50.t = fit.t$coefficients[[1]] + fit.t$coefficients[[2]] * 50
+  # I50.t = (P50.t / 3183.1) * 1000
+  # UCS.t = I50.t * 23
+
+  # regresion robusta
+  fit.r = rlm(P ~ De, data = dat.UCS)
+  # summary(fit.r)
+  P50.r = predict(fit.r, newdata = dat.50)
+  I50.r = (P50.r / 3183.1) * 1000
+  UCS.r = I50.r * 23
+
+  # regresion normal
+  fit.g = lm(P ~ De, data = dat.UCS)
+  # summary(fit.g)
+  P50.g = predict(fit.g, newdata = dat.50)
+  I50.g = (P50.g / 3183.1) * 1000
+  UCS.g = I50.g * 23
+
+  gg = ggplot(dat.UCS, aes(De, P)) +
+    geom_point(size=2) +
+    geom_smooth(method = 'lm', se = F, aes(col='Gaussian'), size=.8) +
+    # geom_line(aes(De, fit.t$fitted.values, col='t-distributed'), size=.8) +
+    geom_smooth(method = 'rlm', se = F, aes(col='Robust'), size=.8) +
+    geom_vline(xintercept = 50, size=1, linetype=8) +
+    theme_bw(base_size = 12) +
+    labs(x='Equivalent diameter, De [mm]',y='Load, P [kN]',col='Method') +
+    scale_color_brewer(type = 'qual', palette = 6) +
+    theme(legend.position = 'top')
+
+  UCS = c(mean(I50), median(I50)) * 23
+
+  UCS.res = data.frame(Method = c('Gaussian','Robust','Corrected_mean','Corrected_median'),
+                       I50 = round(c(I50.g,I50.r,mean(I50),median(I50)),digits = digits),
+                       UCS = round(c(UCS.g,UCS.r,UCS[1],UCS[2]),digits = digits))
+
+  return(list(Graph = gg, Summary = UCS.res))
+}
--- R/pval_chisq.R
+++ R/pval_chisq.R
@@ -15,14 +15,14 @@
 #'
 pval_chisq <- function (s, n, sigma, side = c("two", "one")){
   v = n-1
-  chi = (v * s^2) / sigma
+  chi = (v * s^2) / sigma^2
   if (chi < v) {
     pval = pchisq(chi, v, lower.tail = T) * 2
   } else {
     pval = pchisq(chi, v, lower.tail = F) * 2
   }
   if (any(side == "two")) {
-    pval = (signif(pval, 3))
+    pval = signif(pval, 3)
   } else {
     pval = signif(pval/2, 3)
   }
